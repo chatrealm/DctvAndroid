b8d300d1b359e3f7d21b7bbd887b74492e676cb5
RenAigu <rakruithof@gmail.com>
2016-10-19 18:40:43 +0200

    update cast notification options

--- app/src/main/java/com/tinnvec/dctvandroid/cast/CastOptionsProvider.java
+++ app/src/main/java/com/tinnvec/dctvandroid/cast/CastOptionsProvider.java
@@ -4,6 +4,9 @@ import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
+import com.tinnvec.dctvandroid.PlayStreamActivity;
 import com.tinnvec.dctvandroid.R;
 
 import android.content.Context;
@@ -15,8 +18,16 @@ public class CastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setTargetActivityClassName(PlayStreamActivity.class.getName())
+                .build();
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .build();
+
         return new CastOptions.Builder()
                 .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+                .setCastMediaOptions(mediaOptions)
                 .build();
     }
 
