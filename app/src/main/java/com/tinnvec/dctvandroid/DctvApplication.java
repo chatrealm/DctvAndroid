64dc18638bd2bafbd6505c201151705862683a0e
Kevin <kev@drule.org>
2016-11-10 00:36:21 -0700

    fill in more support for notifications

--- app/src/main/java/com/tinnvec/dctvandroid/DctvApplication.java
+++ app/src/main/java/com/tinnvec/dctvandroid/DctvApplication.java
@@ -3,6 +3,8 @@ package com.tinnvec.dctvandroid;
 import android.app.Application;
 
 import com.onesignal.OneSignal;
+import com.tinnvec.dctvandroid.notifications.DctvNotificationOpenedHandler;
+
 
 /**
  * Created by kev on 11/7/16.
@@ -13,7 +15,10 @@ public class DctvApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        OneSignal.startInit(this).init();
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.NONE);
+        OneSignal.Builder ob = OneSignal.startInit(this);
+        ob.setNotificationOpenedHandler(new DctvNotificationOpenedHandler());
+        ob.init();
     }
 
 }
