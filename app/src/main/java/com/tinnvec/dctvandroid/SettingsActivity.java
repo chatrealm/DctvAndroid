915e3f9c5cab8bd5540663daac55367ad2c3c939
RenAigu <rakruithof@gmail.com>
2016-12-02 01:55:56 +0100

    themed settings, added actionbar

--- app/src/main/java/com/tinnvec/dctvandroid/SettingsActivity.java
+++ app/src/main/java/com/tinnvec/dctvandroid/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.tinnvec.dctvandroid;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by kev on 11/13/16.
@@ -11,10 +15,17 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.content, new SettingsFragment())
                 .commit();
     }
 
