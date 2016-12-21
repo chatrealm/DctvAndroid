2b1abf93ab2c88e95dc91033190cb188f6e27c43
RenAigu <rakruithof@gmail.com>
2016-12-21 18:32:52 +0100

    new art for the 24/7 channel

---
+++ app/src/main/java/com/tinnvec/dctvandroid/tasks/CalendarTask.java
@@ -0,0 +1,14 @@
+package com.tinnvec.dctvandroid.tasks;
+
+import android.os.AsyncTask;
+
+import com.tinnvec.dctvandroid.channel.CalendarEvent;
+
+import java.util.List;
+
+public class CalendarTask extends AsyncTask<Void, Void, List<CalendarEvent>> {
+    @Override
+    protected List<CalendarEvent> doInBackground(Void... params) {
+        return null;
+    }
+}
